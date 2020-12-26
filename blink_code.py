e97bbf99ab3830c2c8a0b9ff1888da48779905da
Andreas Rübner Johnsen <andreas.johnsen@gmail.com>
2020-12-26 18:21:34 +0100

    Update blink_code.py

--- blink_code.py
+++ blink_code.py
@@ -10,5 +10,5 @@ while True:
     led.value = False
     time.sleep(0.1)
     led.value = True
-    # print((random.randint(0, 100), random.randint(-100, 0), random.randint(-50, 50)))
+    # print((random.randint(0, 100), random.randint(-100, 0), random.randint(-50, 50))) # to demonstrate Plotter in mu-editor
     time.sleep(0.5)
