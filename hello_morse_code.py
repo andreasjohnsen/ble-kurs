4b9d948510d6b4196ad149dff433b031a21ba604
Andreas Rübner Johnsen <andreas.johnsen@gmail.com>
2022-01-23 20:18:13 +0100

    Update hello_morse_code.py

--- hello_morse_code.py
+++ hello_morse_code.py
@@ -39,9 +39,9 @@ for char in hello:
         # not valid, just ignore
         pass
 
-while True:
-    led.value = False
-    time.sleep(0.1)
-    led.value = True
-    # print((random.randint(0, 100), random.randint(-100, 0), random.randint(-50, 50))) # to demonstrate Plotter in mu-editor
-    time.sleep(0.5)
+#while True:
+#    led.value = False
+#    time.sleep(0.1)
+#    led.value = True
+#    # print((random.randint(0, 100), random.randint(-100, 0), random.randint(-50, 50))) # to demonstrate Plotter in mu-editor
+#    time.sleep(0.5)
